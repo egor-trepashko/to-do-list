d5d8054833e82db1bd0fb6d6dc80256abb90d23e
egor-trepashko <48236409+egor-trepashko@users.noreply.github.com>
2022-05-11 17:59:36 +0300

    fix bag  v-list style and publicPath

--- src/components/ToDo/ToDoList.vue
+++ src/components/ToDo/ToDoList.vue
@@ -2,11 +2,11 @@
   <div class="list" :class="(TODO_LIST.length == 0) ? 'list_none' : ''">
     <div v-if="TODO_LIST.length > 0" class="list__content">
       <draggable v-model="ToDOList" group="to-do" @start="drag=true" @end="drag=false">
-          <v-slide-y-transition group class="v-list">
-        <template v-for="task of ToDOList">
-          <to-do-item :key="task.key" :status="task.status" :text="task.text" :id="task.key" />
-        </template>
-      </v-slide-y-transition>
+        <v-slide-y-transition group class="v-list">
+          <template v-for="task of ToDOList">
+            <to-do-item :key="task.key" :status="task.status" :text="task.text" :id="task.key" />
+          </template>
+        </v-slide-y-transition>
       </draggable>
     </div>
     <div v-else class="list__none">
@@ -18,7 +18,7 @@
 </template>
 
 <script>
-import draggable from 'vuedraggable'
+import draggable from "vuedraggable";
 import ToDoFooter from "@/components/ToDo/ToDoFooter.vue";
 import ToDoItem from "@/components/ToDo/ToDoItem.vue";
 import { mapGetters, mapActions } from "vuex";
@@ -26,12 +26,12 @@ export default {
   computed: {
     ...mapGetters(["TODO_LIST", "TODO_LIST_VIEW"]),
     ToDOList: {
-        get() {
-            return this.TODO_LIST_VIEW
-        },
-        set(value) {
-            this.DRANG_DROP_SORT(value)
-        }
+      get() {
+        return this.TODO_LIST_VIEW;
+      },
+      set(value) {
+        this.DRANG_DROP_SORT(value);
+      }
     }
   },
   methods: {
@@ -68,6 +68,10 @@ export default {
     min-height: 130px;
     max-height: calc(100vh - 816px);
     overflow-y: scroll;
+    display: block;
+    padding: 8px 0;
+    position: static;
+    transition: box-shadow 280ms cubic-bezier(0.4, 0, 0.2, 1);
   }
 }
 </style>
